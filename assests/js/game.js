6967df8b15e54da0a64d788fe3a332c2bf5dcdeb
ryeon <ryeonnaderi@gmail.com>
2018-06-20 17:16:21 -0400

    adding comments

---
+++ assests/js/game.js
@@ -0,0 +1,5 @@
+//a word is made
+var wordList = []
+// user has to pck a letter that corresponds to a letter from the word 
+// if the letter is right, then it get placed on one of the line
+//if they get it wrong, then they lose one of thier lives and when they lose all 6 lives, then game over 
