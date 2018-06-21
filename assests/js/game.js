5ca00b4773eb72587ee80769a67b5e426de8dbad
ryeon <ryeonnaderi@gmail.com>
2018-06-21 19:01:37 -0400

    added background and random word function

--- assests/js/game.js
+++ assests/js/game.js
@@ -1,5 +1,21 @@
 //a word is made
-var wordList = []
+var wordList = ["citadel of ricks","morty","jerry","beth","portal gun","wubba lubba dub dub","peace among worlds", "get schwift", "awwww geeez", "gazorpazorpfield","natomy park","mr meseeks", "tiny rick","pickle rick"]
+
+var randomWord =function()
+{
+    return wordList[Math.floor(Math.random() * wordList.length)];
+};
+
+var Word = randomWord();
+console.log(Word);
+
+var lives =5;
+
+for (i = 0; i <= Word.length; i++) 
+{ 
+    text i ="-";
+}
+console.log(spaces);
 // user has to pck a letter that corresponds to a letter from the word 
 // if the letter is right, then it get placed on one of the line
 //if they get it wrong, then they lose one of thier lives and when they lose all 6 lives, then game over 
