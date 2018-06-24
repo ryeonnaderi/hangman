975222df9505ecc03bfe735714141776540c6f6e
ryeon <ryeonnaderi@gmail.com>
2018-06-24 11:07:24 -0400

    added thye psudo code and added lives with if they miss a letter

--- assests/js/game.js
+++ assests/js/game.js
@@ -1,6 +1,6 @@
 //a word is made
-var wordList = ["citadel of ricks","morty","jerry","beth","portal gun","wubba lubba dub dub","peace among worlds", "get schwift", "awwww geeez", "gazorpazorpfield","natomy park","mr meseeks", "tiny rick","pickle rick"]
-
+var wordList = ["summer","citadel of ricks","morty","jerry","beth","portal gun","wubba lubba dub dub","peace among worlds", "get schwift", "awwww geeez", "gazorpazorpfield","anatomy park","mr meseeks", "tiny rick","pickle rick"]
+// random word is choosen
 var randomWord =function()
 {
     return wordList[Math.floor(Math.random() * wordList.length)];
@@ -9,7 +9,7 @@ var randomWord =function()
 var Word = randomWord();
 console.log(Word);
 
-var lives =5;
+// the spaces are made 
 var spaces = [];
 
 for (i = 0; i <= Word.length; i++) 
@@ -27,14 +27,27 @@ var answer =[];
 for (var i = 0; i <Word.length; i++)
 {
     answer[i] = Word.charAt(i);
+    
     console.log(answer);
 }
-    
+   
+// keyboard prints letters being pressed 
 document.onkeyup = function(event)
 {
     e = e|| window.event;
     var input = e.input;
-    // character = string.fromCharcode(charcode)
+    character = string.fromCharcode(charcode)
 
     console.log(charcode);
-}  
+}
+
+// created lives
+var lives =5;
+// if they miss a letter
+if (input = word.charAt)
+{
+    lives--;
+    // console.log(lives);
+}
+
+
